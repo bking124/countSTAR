619439707dbf4c08ee7563eba675ed1f1e9a6e80
Brian King <brianking387@gmail.com>
2023-06-29 14:23:20 -0500

    Add roaches dataset to package

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Data on the efficacy of a pest management system at reducing the number of
+#' roaches in urban apartments.
+#'
+#'
+#' @format ## `roaches`
+#' A data frame with 262 obs. of 6 variables:
+#' \describe{
+#' \item{y} Number of roaches caught
+#' \item{roach1} Pretreatment number of roaches
+#' \item{treatment} Treatment indicator
+#' \item{senior} Indicator for only elderly residents in building
+#' \item{exposure2} Number of days for which the roach traps were used
+#' }
+#' @source Gelman and Hill (2007); package `rstanarm`
+"roaches"
